4a519f92b69735e359090f19c01d038b887bca05
1LightHouse1 <vini_nascimento1@outlook.com>
2023-08-21 22:10:20 -0300

    teste

--- axios/axios.js
+++
@@ -1,8 +0,0 @@
-axios.get('https://api.exchangerate-api.com/v4/lastest/USD')
-    .then(function(response){
-
-        console.log(response)
-    })
-    .catch(function(error){
-        //lidar com erros
-    })
